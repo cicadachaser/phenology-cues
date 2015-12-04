5e0197e6535ae7e3f4e9148fcb8553890be4d73b
cicadachaser <cicadachaser@gmail.com>
2015-12-03 16:31:48 -0800

    reverted to previous master from local archive and added a few lines to truncate the dataset to 1914 to 2014.

--- windows_main.R
+++ windows_main.R
@@ -6,7 +6,7 @@ rm(list=ls())
 
 #libraries
 library(timeDate)
-library(Cairo) #I'm not sure if we need this with the plotting removed
+library(Amelia)
 
 #Set appropriate working directory
 if(Sys.getenv("USERNAME")=="Collin" || Sys.getenv("USERNAME")=="collin"){ #If it's collin
@@ -51,49 +51,30 @@ mutrate<-data.frame( #probability of each trait mutating in an individual. Mutat
 
 #input data
 davis.daily<-read.csv("davis-data/626713.csv", header=T, na.strings="-9999")
+davis.daily$YEAR<-as.numeric(substr(davis.daily$DATE,1,4)) #simple field for year
+davis.daily<-davis.daily[davis.daily$YEAR>1913 & davis.daily$YEAR<2015,] #truncates the data to 101 complete years between 1914 and 2014
+
 davis.daily$PRCP<-davis.daily$PRCP/10 #precips are reported in tenths of mm
 davis.daily$TMAX<-davis.daily$TMAX/10 #temps are reported in tenths of degree C
 davis.daily$TMIN<-davis.daily$TMIN/10 #temps are reported in tenths of degree C
 davis.daily$DATE2<-as.Date(as.character(davis.daily$DATE),format="%Y %m %d") #DATE2 is date formatted
-davis.daily$JULIAN<-julian(davis.daily$DATE2,origin=as.Date("1892-12-31")) #1893-01-01 is day 1...
-davis.daily$YEAR<-as.numeric(substr(davis.daily$DATE,1,4)) #simple field for year
+davis.daily$JULIAN<-julian(davis.daily$DATE2,origin=as.Date("1913-12-31")) #1914-01-01 is day 1
 davis.daily$MONTH<-as.numeric(substr(davis.daily$DATE,5,6)) #simple field for month
 davis.daily$DAY<-as.numeric(substr(davis.daily$DATE,7,8)) #simple field for day
 davis.daily<-davis.daily[,c("DATE2","JULIAN", "YEAR","MONTH","DAY","PRCP","TMAX","TMIN")] #simplified dataframe
 
 davis.yearlist<-split(davis.daily,davis.daily$YEAR) #list of each year separated
+
+davis.yearnames<-unique(davis.daily$YEAR) #gives a list of all the years in the data
+
 #calculates the "day of year", i.e. Jan 1 is 1, and 12/31 is 365 
 #adds a DAY.OF.YEAR column to each dataframe in the year list
-davis.yearnames<-unique(davis.daily$YEAR)
 for (i in 1:length(davis.yearnames)){
   davis.yearlist[[i]]$DAY.OF.YEAR<-julian(davis.yearlist[[i]]$DATE2, origin=as.Date(paste(davis.yearnames[i],"01","01",sep="-")))+1 #add +1 so that the first day of the year is 1, not zero. 
 }
-yearlist.store=davis.yearlist
-goodyears=NULL
 
-for(iyear in davis.yearnames){
-  nacount=sum(sum(is.na(davis.yearlist[[as.character(iyear)]])))
-  daycount=dim(davis.yearlist[[as.character(iyear)]])[1]
-  if(nacount==0 & daycount>364){goodyears=c(goodyears,iyear)}
-}
-davis.yearlist=davis.yearlist[as.character(goodyears)]
-
-davis.yearnames<-goodyears #gives a list of all the years in the data
-
-
-
-
-davis.daily<-unsplit(yearlist.store,davis.daily$YEAR) #using legacy "yearlist.store" to make unsplit happy
-# DAY.OF.YEAR=rep(0,dim(davis.daily)[1])
-# for(i in 1:length(DAY.OF.YEAR)){
-#   DAY.OF.YEAR[i]=sprintf("%02d%02d",davis.daily[i,"MONTH"],davis.daily[i,"DAY"])
-#   
-# }
-
-# davis.daily=cbind(davis.daily, DAY.OF.YEAR)
-
-# davis.daily<-unsplit(davis.daily,davis.daily$YEAR)
-davis.daily.means<-aggregate(cbind(TMAX,TMIN,PRCP)~DAY.OF.YEAR, data=davis.daily[davis.daily$YEAR %in% goodyears,], mean)
+davis.daily<-unsplit(davis.yearlist,davis.daily$YEAR)
+davis.daily.means<-aggregate(cbind(TMAX,TMIN,PRCP)~DAY.OF.YEAR, data=davis.daily, mean)
 
 davis.yearvar<-data.frame(row.names=davis.yearnames) #dataframe to hold environmental variability
 
@@ -120,6 +101,10 @@ for (i in 1:length(davis.yearnames)){
   davis.yearvar[i,"TMIN.DEL"]<-sum(comparison$TMIN.x-comparison$TMIN.y,na.rm=T)/davis.yearvar[i,"TMIN.N"]
   davis.yearvar[i,"PRCP.DEL"]<-sum(comparison$PRCP.x-comparison$PRCP.y,na.rm=T)/davis.yearvar[i,"PRCP.N"]
 }
+
+plot(davis.daily$JULIAN,davis.daily$TMAX)
+
+# END IMPUTATION BRANCH REVISIONS
 
 ######################################################
 # Import sequence of years - LOUIE'S STUFF GOES HERE #
