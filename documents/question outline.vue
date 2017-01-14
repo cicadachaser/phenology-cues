2edd9c16c6b4f61864d4c90f264a39a6766cf1ef
cicadachaser <lhyang@ucdavis.edu>
2017-01-13 17:12:26 -0800

    updated envirohistories

---
+++ documents/question outline.vue
@@ -0,0 +1,249 @@
+<!-- Tufts VUE 3.3.0 concept-map (question outline.vue) 2017-01-13 -->
+<!-- Tufts VUE: http://vue.tufts.edu/ -->
+<!-- Do Not Remove: VUE mapping @version(1.1) jar:file:/C:/Program%20Files%20(x86)/VUE/VUE.jar!/tufts/vue/resources/lw_mapping_1_1.xml -->
+<!-- Do Not Remove: Saved date Fri Jan 13 14:31:50 PST 2017 by louie on platform Windows 8 6.2 in JVM 1.7.0_21-b11 -->
+<!-- Do Not Remove: Saving version @(#)VUE: built October 8 2015 at 1658 by tomadm on Linux 2.6.32-504.23.4.el6.x86_64 i386 JVM 1.7.0_21-b11(bits=32) -->
+<?xml version="1.0" encoding="US-ASCII"?>
+<LW-MAP xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xsi:noNamespaceSchemaLocation="none" ID="0"
+    label="question outline.vue" created="1484344537197" x="0.0" y="0.0"
+    width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+    <resource referenceCreated="1484346710266"
+        spec="C:\Users\louie\Documents\GitHub\phenology-cues\documents\question outline.vue"
+        type="1" xsi:type="URLResource">
+        <title>question outline.vue</title>
+        <property key="File" value="C:\Users\louie\Documents\GitHub\phenology-cues\documents\question outline.vue"/>
+    </resource>
+    <fillColor>#FFFFFF</fillColor>
+    <strokeColor>#404040</strokeColor>
+    <textColor>#000000</textColor>
+    <font>SansSerif-plain-14</font>
+    <URIString>http://vue.tufts.edu/rdf/resource/99f55909a9ed4d2e01aac82132cc453d</URIString>
+    <child ID="6"
+        label="Simulations manipulating environmental variability"
+        layerID="1" created="1484344542086" x="-448.5" y="22.0"
+        width="470.0" height="33.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#FC938D</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-21</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f55909a9ed4d2e01aac8213bb10d33</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="10" label="methods subsampling environmental data"
+        layerID="1" created="1484344677499" x="-638.8333" y="432.83337"
+        width="429.75" height="119.25" strokeWidth="1.0"
+        autoSized="true" xsi:type="node">
+        <fillColor>#33A8F5</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-21</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f55909a9ed4d2e01aac8216effdde1</URIString>
+        <child ID="16"
+            label="choosing sets of years with high year-to-year variation"
+            created="1484344746586" x="34.0" y="33.0" width="520.0"
+            height="33.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+            <fillColor>#33A8F5</fillColor>
+            <strokeColor>#000000</strokeColor>
+            <textColor>#000000</textColor>
+            <font>Arial-plain-21</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/99f55909a9ed4d2e01aac821a26b7afd</URIString>
+            <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+        </child>
+        <child ID="12"
+            label="choosing years with high day-to-day variation"
+            created="1484344711874" x="34.0" y="60.75" width="434.0"
+            height="33.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+            <fillColor>#33A8F5</fillColor>
+            <strokeColor>#000000</strokeColor>
+            <textColor>#000000</textColor>
+            <font>Arial-plain-21</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/99f55909a9ed4d2e01aac8216951927a</URIString>
+            <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+        </child>
+        <child ID="18"
+            label="choosing locations with higher/lower variation"
+            created="1484344774912" x="34.0" y="88.5" width="434.0"
+            height="33.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+            <fillColor>#33A8F5</fillColor>
+            <strokeColor>#000000</strokeColor>
+            <textColor>#000000</textColor>
+            <font>Arial-plain-21</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/99f5591aa9ed4d2e01aac8219ceb36c8</URIString>
+            <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+        </child>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="11" label="Q1" layerID="1" created="1484344677500"
+        x="-396.81573" y="54.5" width="176.16621" height="378.83334"
+        strokeWidth="1.0" strokeStyle="4" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-32</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f5591aa9ed4d2e01aac8215009c862</URIString>
+        <point1 x="-221.14952" y="55.0"/>
+        <point2 x="-396.31573" y="432.83334"/>
+        <ID1 xsi:type="node">6</ID1>
+        <ID2 xsi:type="node">10</ID2>
+    </child>
+    <child ID="20" label="simulating environmental variation"
+        layerID="1" created="1484344793887" x="-77.16669" y="304.16666"
+        width="354.0" height="91.5" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#F2AE45</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-21</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f5591ba9ed4d2e01aac8212be3ed33</URIString>
+        <child ID="23" label="increasing year-to-year variation"
+            created="1484344827119" x="34.0" y="33.0" width="316.0"
+            height="33.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+            <fillColor>#F2AE45</fillColor>
+            <strokeColor>#000000</strokeColor>
+            <textColor>#000000</textColor>
+            <font>Arial-plain-21</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/99f5591ba9ed4d2e01aac82174132cf0</URIString>
+            <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+        </child>
+        <child ID="8" label="increasing day-to-day variation"
+            created="1484344639540" x="34.0" y="60.75" width="300.0"
+            height="33.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+            <fillColor>#F2AE45</fillColor>
+            <strokeColor>#000000</strokeColor>
+            <textColor>#000000</textColor>
+            <font>Arial-plain-21</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/99f5591ca9ed4d2e01aac8213d03dedb</URIString>
+            <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+        </child>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="21" label="Q2" layerID="1" created="1484344793889"
+        x="-197.39845" y="54.5" width="251.70016" height="250.16666"
+        strokeWidth="1.0" strokeStyle="4" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-32</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f5591da9ed4d2e01aac8215febfcc7</URIString>
+        <point1 x="-196.89845" y="55.0"/>
+        <point2 x="53.801712" y="304.16666"/>
+        <ID1 xsi:type="node">6</ID1>
+        <ID2 xsi:type="node">20</ID2>
+    </child>
+    <child ID="25"
+        label="simulations manipulating environmental history (Env 1 > Env 2)"
+        layerID="1" created="1484344913650" x="595.16656" y="28.666664"
+        width="593.0" height="35.0" strokeWidth="1.0" autoSized="false" xsi:type="node">
+        <fillColor>#FC938D</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-21</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f5591ea9ed4d2e01aac821d0d39f45</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="27" label="methods subsampling environmental data"
+        layerID="1" created="1484345039364" x="475.49933" y="184.69861"
+        width="393.0" height="33.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#33A8F5</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-21</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f5591ea9ed4d2e01aac821f7ccd360</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="28" label="Q3" layerID="1" created="1484345039366"
+        x="694.8784" y="63.166687" width="172.49213" height="122.03192"
+        strokeWidth="1.0" strokeStyle="4" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-32</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f5591fa9ed4d2e01aac821c2149edb</URIString>
+        <point1 x="866.87054" y="63.666687"/>
+        <point2 x="695.3784" y="184.69861"/>
+        <ID1 xsi:type="node">25</ID1>
+        <ID2 xsi:type="node">27</ID2>
+    </child>
+    <child ID="29" label="simulating environmental histories"
+        layerID="1" created="1484345049376" x="1015.4994" y="170.03195"
+        width="325.0" height="33.0" strokeWidth="1.0" autoSized="true" xsi:type="node">
+        <fillColor>#F2AE45</fillColor>
+        <strokeColor>#000000</strokeColor>
+        <textColor>#000000</textColor>
+        <font>Arial-plain-21</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f55920a9ed4d2e01aac821a89e87dd</URIString>
+        <shape arcwidth="20.0" archeight="20.0" xsi:type="roundRect"/>
+    </child>
+    <child ID="30" label="Q4" layerID="1" created="1484345049378"
+        x="926.865" y="63.166656" width="217.97577" height="107.365295"
+        strokeWidth="1.0" strokeStyle="4" autoSized="false"
+        controlCount="0" arrowState="2" xsi:type="link">
+        <strokeColor>#404040</strokeColor>
+        <textColor>#404040</textColor>
+        <font>Arial-plain-32</font>
+        <URIString>http://vue.tufts.edu/rdf/resource/99f55920a9ed4d2e01aac82120afc5bb</URIString>
+        <point1 x="927.36505" y="63.666656"/>
+        <point2 x="1144.3408" y="170.03195"/>
+        <ID1 xsi:type="node">25</ID1>
+        <ID2 xsi:type="node">29</ID2>
+    </child>
+    <layer ID="1" label="Layer 1" created="1484344537211" x="0.0"
+        y="0.0" width="1.4E-45" height="1.4E-45" strokeWidth="0.0" autoSized="false">
+        <URIString>http://vue.tufts.edu/rdf/resource/99f55920a9ed4d2e01aac821e577c01b</URIString>
+    </layer>
+    <userZoom>0.75</userZoom>
+    <userOrigin x="-715.2505" y="-232.10104"/>
+    <presentationBackground>#202020</presentationBackground>
+    <PathwayList currentPathway="0" revealerIndex="-1">
+        <pathway ID="0" label="Untitled Pathway" created="1484344537196"
+            x="0.0" y="0.0" width="1.4E-45" height="1.4E-45"
+            strokeWidth="0.0" autoSized="false" currentIndex="-1" open="true">
+            <strokeColor>#B3993333</strokeColor>
+            <textColor>#000000</textColor>
+            <font>SansSerif-plain-14</font>
+            <URIString>http://vue.tufts.edu/rdf/resource/99f55920a9ed4d2e01aac8210613a4b1</URIString>
+            <masterSlide ID="2" created="1484344537238" x="0.0" y="0.0"
+                width="800.0" height="600.0" locked="true"
+                strokeWidth="0.0" autoSized="false">
+                <fillColor>#000000</fillColor>
+                <strokeColor>#404040</strokeColor>
+                <textColor>#000000</textColor>
+                <font>SansSerif-plain-14</font>
+                <URIString>http://vue.tufts.edu/rdf/resource/99f55920a9ed4d2e01aac821b08e0422</URIString>
+                <titleStyle ID="3" label="Header"
+                    created="1484344537255" x="335.5" y="172.5"
+                    width="129.0" height="55.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-36</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/99f55920a9ed4d2e01aac821d0c41f29</URIString>
+                    <shape xsi:type="rectangle"/>
+                </titleStyle>
+                <textStyle ID="4" label="Slide Text"
+                    created="1484344537256" x="346.5" y="281.5"
+                    width="107.0" height="37.0" strokeWidth="0.0"
+                    autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#FFFFFF</textColor>
+                    <font>Gill Sans-plain-22</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/99f55920a9ed4d2e01aac821dffad89f</URIString>
+                    <shape xsi:type="rectangle"/>
+                </textStyle>
+                <linkStyle ID="5" label="Links" created="1484344537256"
+                    x="373.5" y="384.0" width="53.0" height="32.0"
+                    strokeWidth="0.0" autoSized="true" isStyle="true" xsi:type="node">
+                    <strokeColor>#404040</strokeColor>
+                    <textColor>#B3BFE3</textColor>
+                    <font>Gill Sans-plain-18</font>
+                    <URIString>http://vue.tufts.edu/rdf/resource/99f55920a9ed4d2e01aac8217b0774bc</URIString>
+                    <shape xsi:type="rectangle"/>
+                </linkStyle>
+            </masterSlide>
+        </pathway>
+    </PathwayList>
+    <date>2017-01-13</date>
+    <modelVersion>6</modelVersion>
+    <saveLocation>C:\Users\louie\Documents\GitHub\phenology-cues\documents</saveLocation>
+    <saveFile>C:\Users\louie\Documents\GitHub\phenology-cues\documents\question outline.vue</saveFile>
+</LW-MAP>
